2686b2d55b30a195be0d772544c04cebcd034491
AntonewtonQ <antonewtonquima@gmail.com>
2025-05-11 09:05:29 +0100

    analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@
 
 import AboutSection from "@/components/about";
 import Footer from "@/components/footer";
-
 import HeroWithHeader from "@/components/hero";
-import HowItWorksSection from "@/components/faqs";
 import ServicesSection from "@/components/services";
 import FAQSection from "@/components/faqs";
 import CallToActionSection from "@/components/call";
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50">
